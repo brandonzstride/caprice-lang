77151aeef24f4d4fc5b68ac1524e636dc31f7bd4
Brandon Stride <brandonzstride@hotmail.com>
2025-12-08 11:55:21 -0500

    more input env

--- src/interp/input.ml
+++ src/interp/input.ml
@@ -4,3 +4,15 @@ type t =
   | IInt of int
   | ILabel of Label.t
   [@@deriving eq, ord]
+
+let bool_opt = function
+  | IBool b -> Some b
+  | _ -> None
+
+let int_opt = function
+  | IInt i -> Some i
+  | _ -> None
+
+let label_opt = function
+  | ILabel l -> Some l
+  | _ -> None
--- src/interp/input_env.ml
+++ src/interp/input_env.ml
@@ -8,4 +8,23 @@ module Make (K : Smt.Symbol.KEY) = struct
   let empty : t = M.empty
 
   let find : K.t -> t -> Input.t option = M.find_opt
+
+  let find_and_bind (f : Input.t -> 'a option) (k : K.t) (m : t) : 'a option =
+    Option.bind (find k m) f
+
+  let find_bool_opt : K.t -> t -> bool option = find_and_bind Input.bool_opt
+  let find_int_opt : K.t -> t -> int option = find_and_bind Input.int_opt
+  let find_label_opt : K.t -> t -> Label.t option = find_and_bind Input.label_opt
+
+  let add : K.t -> Input.t -> t -> t = M.add
+
+  (**
+    [remove_greater max_key t] is the map [t] filtered to only have keys not
+    exceeding [max_key].
+  *)
+  let remove_greater (max_key : K.t) (m : t) : t =
+    let new_m, i_opt, _ = M.split max_key m in
+    match i_opt with
+    | Some i -> M.add max_key i new_m
+    | None -> new_m
 end
