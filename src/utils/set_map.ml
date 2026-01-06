3d432af8fd123894b329d7ca0162a4d1c3c40f5c
Brandon Stride <brandonzstride@hotmail.com>
2026-01-06 12:51:35 -0500

    refactor random from map and set

--- src/utils/set_map.ml
+++ src/utils/set_map.ml
@@ -2,32 +2,28 @@
 module Make_H (K : Baby.OrderedType) = struct
   module B = Baby.H.Make (K)
 
+  (* TODO: make this faster by actually walking the tree. However
+    that would require I modify the source code of Baby. *)
+  let random_from_seq ~size seq =
+    let n = ref size in
+    Seq.find (fun _ ->
+      let i = Random.int !n in
+      n := !n - 1;
+      i = 0
+    ) seq
+
   module Map = struct
     include B.Map
     let domain = B.domain
 
-    (* TODO: make this faster by actually walking the tree. However
-      that would require I modify the source code. *)
     let random_binding_opt (m : 'a t) : (K.t * 'a) option =
-      let n = ref (cardinal m) in
-      to_seq m
-      |> Seq.find (fun _ ->
-        let i = Random.int !n in
-        n := !n - 1;
-        i = 0
-      )
+      random_from_seq ~size:(cardinal m) (to_seq m)
   end
 
   module Set = struct
     include B.Set
 
-    let random_opt (s : t) : elt option =
-      let n = ref (cardinal s) in
-      to_seq s
-      |> Seq.find (fun _ ->
-        let i = Random.int !n in
-        n := !n - 1;
-        i = 0
-      )
+    let random_elt_opt (s : t) : elt option =
+      random_from_seq ~size:(cardinal s) (to_seq s)
   end
 end
